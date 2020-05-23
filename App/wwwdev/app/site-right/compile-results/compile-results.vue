56ed30950040baa9cca55b9de253b0281cf66443
Casey Marquis <caseymarquis@gmail.com>
2020-05-22 22:56:37 -0400

    WIP: Compile result

--- App/wwwdev/app/site-right/compile-results/compile-results.vue
+++ App/wwwdev/app/site-right/compile-results/compile-results.vue
@@ -1,12 +1,91 @@
 <template>
-    <div>
-        <!-- TODO: Display something about the file here. This is especially useful if the compile has failed. -->
+    <div v-if="testFile !== null" class="test-file">
+        <div class="header-container">
+            <h2 class="header">
+                <span class="header-bright" v-text="testFile.fileName"></span>
+            </h2>
+        </div>
+        <pre v-text="result.output"></pre>
     </div>
 </template>
 
 <script>
-export default {};
+import Updates from "../../js/Updates.js";
+import api from "../../js/api.js";
+
+export default {
+    data() {
+        return {
+            testFile: null
+        };
+    },
+    created() {
+        this.fetchData();
+    },
+    watch: {
+        $route: "fetchData"
+    },
+    computed: {
+        result() {
+            return (this.testFile && this.testFile.compileResult) || {};
+        }
+    },
+    methods: {
+        fetchData() {
+            api.get(`RootSourceFile/${this.$route.params.pathHash}`).then(
+                testFile => {
+                    this.testFile = testFile;
+                }
+            );
+        }
+    },
+    components: {}
+};
 </script>
 
-<style>
+<style scoped>
+.test-file {
+    display: flex;
+    flex-flow: column nowrap;
+}
+
+.header-container {
+    background-color: #212121;
+    width: 100%;
+    border-bottom: double;
+    padding-left: 1em;
+}
+
+.header {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+}
+
+.header-bright {
+    color: white;
+}
+
+.code-container {
+    overflow: scroll;
+    flex-grow: 1;
+
+    display: flex;
+    flex-flow: row nowrap;
+}
+
+.number-container {
+    height: fit-content;
+    background-color: #121417;
+    border-right: solid;
+    border-color: #696969;
+    padding-right: 0.25em;
+}
+
+.line-container {
+    height: fit-content;
+    flex-grow: 1;
+    padding-left: 0.25em;
+    background-color: #121417;
+}
 </style>
